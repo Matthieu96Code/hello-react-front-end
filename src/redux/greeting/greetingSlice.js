91a36ba9739080f12c6ec85bb840614e681cd720
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-07-28 06:13:47 +0000

    Create slice and store

---
+++ src/redux/greeting/greetingSlice.js
@@ -0,0 +1,54 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+// export const fetchGreeting = createAsyncThunk(
+//   'greeting/fetchGreeting',
+//   async () => {
+//     const response = await fetch('http://127.0.0.1:3000/api/messages');
+//     const data = await response.json();
+//     if (response.status < 200 || response.status >= 300) {
+//       return 'fails';
+//     }
+//     return data;
+//   },
+// );
+
+export const fetchGreeting = createAsyncThunk(
+  'greetings/fetchGreeting',
+  async () => {
+    try {
+      const res = await fetch('http://localhost:3000/api/messages');
+      const data = await res.json();
+      return data.greeting;
+    } catch (e) {
+      return e.message;
+    }
+  },
+);
+
+const initialState = {
+  message: '',
+  isLoading: true,
+};
+
+export const GreetingSlice = createSlice({
+  name: 'greeting',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGreeting.pending, (state) => ({
+        ...state,
+        isLoading: true,
+      }))
+      .addCase(fetchGreeting.fulfilled, (state, action) => ({
+        ...state,
+        isLoading: false,
+        message: action.payload,
+      }))
+      .addCase(fetchGreeting.rejected, (state) => ({
+        ...state,
+        isLoading: false,
+      }));
+  },
+});
+
+export default GreetingSlice.reducer;
