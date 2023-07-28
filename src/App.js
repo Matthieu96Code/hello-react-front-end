d2c3043f96b1ec53731cc1039b14087c54f4ed0c
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-07-28 06:14:38 +0000

    Create app and greeting components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import Greeting from './components/Greeting';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<Greeting />} />),
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
